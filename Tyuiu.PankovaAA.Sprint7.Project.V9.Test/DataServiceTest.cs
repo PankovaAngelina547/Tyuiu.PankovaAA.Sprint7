a1de968d0fea5e3d8661c12417e8fd1925dd6d90
Ангелина Панькова <Tyuiu.PankovaAA@outlook.com>
2025-12-16 18:01:09 +0500

    Add project files.

---
+++ Tyuiu.PankovaAA.Sprint7.Project.V9.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.PankovaAA.Sprint7.Project.V9.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.PankovaAA.Sprint7.Project.V9/Form1.cs
@@ -0,0 +1,10 @@
+namespace Tyuiu.PankovaAA.Sprint7.Project.V9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
